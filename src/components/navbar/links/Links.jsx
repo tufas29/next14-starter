22e1fb835b0d73f67386a37da31fe1896e516910
Shmuelt <tufas29@gmail.com>
2024-02-26 06:40:14 +0200

    2th day

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,4 +1,4 @@
-import styles from "./links.module.css"
+import styles from "./links.module.css";
 import NavLink from "./navLink";
 const links = [
   {
@@ -19,12 +19,23 @@ const links = [
   },
 ];
 
+const session = true;
+const isAdmin = true;
+
 const Links = () => {
   return (
     <div className={styles.links}>
       {links.map((link) => (
         <NavLink item={link} />
       ))}
+      {session ? (
+        <>
+          {isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }} />}
+          <button className={styles.logout}>Logout</button>
+        </>
+      ) : (
+        <NavLink item={{ title: "Login", path: "/Login" }} />
+      )}
     </div>
   );
 };
